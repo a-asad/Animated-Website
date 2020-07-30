a81d41730ba2863599cccef34f284411a2e2c99e
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-07-30 23:44:59 +0500

    //added animations on scroll.. // using aos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useWebAnimations ,{slideInDown, flipInX, bounceIn, tada} from "@wellyshen/use-web-animations";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 import './App.css';
 import p1 from './a3.png';
 import p2 from './a1.png';
@@ -10,6 +12,9 @@ import p6 from './a6.png';
 import logo from './logo.png';
 
 function App() {
+  useEffect(()=>{
+   AOS.init({duration:2500});
+  },[])
   const navAnim = useWebAnimations({...slideInDown});
   const p2Anim = useWebAnimations({
     keyframes:flipInX.keyframes,
@@ -66,16 +71,16 @@ function App() {
 
       <div className='main text' style={{marginTop:60}}>
       <img src={p3} alt='illustration' ref={img1.ref} style={{minWidth:'300px', maxWidth:'40vw'}}/>
-        <div style={{minWidth:'300px', maxWidth:'34vw'}}>
+        <div data-aos='fade-left' style={{minWidth:'300px', maxWidth:'34vw'}}>
           <h1>Innovative and intelligent
           technology solutions
           and services.</h1>
-          <div style={{textAlign:"center"}}><button>Product Management</button></div>
+          <div style={{textAlign:"center"}} data-aos='slide-right'><button>Product Management</button></div>
         </div>
       </div>
 
       <div className='section1'>
-        <div style={{textAlign:"center"}}>
+        <div style={{textAlign:"center"}} data-aos='flip-right'>
           <img src={p2} ref={p2Anim.ref} alt='illustration' style={{minWidth:'300px', maxWidth:'40vw'}}/>
           <div style={{minWidth:'300px', maxWidth:'40vw', color:'#ed9600'}}>
             <h1>We're here to help you
@@ -83,7 +88,7 @@ function App() {
             <div style={{textAlign:"center"}}><button>Let's Make it Happen</button></div>
           </div>
         </div>
-        <div style={{textAlign:"center"}}>
+        <div style={{textAlign:"center"}} data-aos='flip-left'>
           <img src={p1} ref={p3Anim.ref} alt='illustration' style={{minWidth:'300px', maxWidth:'35vw'}}/>
           <div style={{minWidth:'300px', maxWidth:'40vw', color:'#ed9600'}}>
             <h1>Scalable extended
@@ -95,23 +100,23 @@ function App() {
       </div>
        
       <div className='section2 text'>
-        <div style={{minWidth:'300px', maxWidth:'34vw'}}>
+        <div style={{minWidth:'300px', maxWidth:'34vw'}} data-aos='fade-right'>
           <h1>Customized solutions
               to meet the needs of
               the changing market</h1>
-          <div style={{textAlign:"center"}}><button>Revenue Cycle Management</button></div>
+          <div style={{textAlign:"center"}}  data-aos='slide-right'><button>Revenue Cycle Management</button></div>
         </div>
         <img src={p4} ref={p4Anim.ref} alt='illustration' style={{minWidth:'300px', maxWidth:'40vw'}}/>
       </div>
 
-      <div>
-        <h1 style={{textAlign:"center", color:'#ed9600'}}>Our Featured Services</h1>
+      <div >
+        <h1 style={{textAlign:"center", color:'#ed9600'}} data-aos='fade-up'>Our Featured Services</h1>
         <div className='section3 text'>
-          <div className='cols'>
+          <div className='cols' data-aos='flip-up'>
             <img src={p5} alt='illustration' style={{width:'100%'}}/>
             <h2 style={{textAlign:"center"}}>Automation with Cloud Infrastructure</h2>
           </div>
-          <div className='cols'>
+          <div className='cols' data-aos='flip-up'>
             <img src={p6} alt='illustration' style={{width:'100%'}}/>
             <h2 style={{textAlign:"center"}}>Internet of Things for End User Applications</h2>
           </div>
